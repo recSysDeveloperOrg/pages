c4417f84d81b646c7c2c84c6ff45b126d1f64f43
lijunyu <lijunyu@ljygogogo.com>
2021-12-27 21:10:41 +0800

    revise ip for production

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -55,6 +55,6 @@ class Fetch implements HttpRequest {
 
 }
 
-const localFetch = new Fetch("localhost", 8080, 1)
+const localFetch = new Fetch("172.17.168.4", 49999, 1)
 export {localFetch, ResolveBaseResponse, ResponseStatus};
 export type { Response };
